a3cd8eb360657c3c2b6706e02209792a59f11ec3
Benoit Challat <bchallat@f6r4s2.clusters.42paris.fr>
2025-09-23 19:13:09 +0200

    write newZombie fonction

--- module01/ex00/newZombie.cpp
+++ module01/ex00/newZombie.cpp
@@ -10,4 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
 
+Zombie	*newZombie( std::string name)
+{
+	Zombie *zombie = NULL;
+	zombie = new Zombie(name);
+	return (zombie);
+}
