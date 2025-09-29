8dbbd6d2af3c1968418d9863a226439836a56360
Benoit Challat <bchallat@f6r4s5.clusters.42paris.fr>
2025-09-29 10:46:01 +0200

    import file to ex00 & create zombieHorde file

---
+++ module01/ex01/Zombie.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchallat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 10:49:57 by bchallat          #+#    #+#             */
+/*   Updated: 2025/09/23 10:49:59 by bchallat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie( std::string name) : name(name)
+{
+	std::cout << "Zombie " << name << " create"<<std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << name << "BraiiiiiiinnnnnzZ ..." << std::endl;
+}
+
+Zombie::~Zombie( void )
+{
+	std::cout << "Zombie " << name << " dead" << std::endl;
+}
---
+++ module01/ex01/Zombie.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchallat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 10:50:29 by bchallat          #+#    #+#             */
+/*   Updated: 2025/09/23 10:50:32 by bchallat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+
+class	Zombie {
+
+	private:
+		std::string	name;
+	public:
+		Zombie( std::string name );
+		void	announce( void );
+		~Zombie( void );
+};
+
+/*----------------------------------------------------*/
+
+Zombie	*newZombie( std::string name );
+void	randomChump( std::string name );
+
+#endif
