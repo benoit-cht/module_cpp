94bf643ac7e409de20bf36f04c576e0dc3a37049
Benoit Challat <bchallat@f1br7s17.clusters.42paris.fr>
2025-10-05 12:16:26 +0200

    write a main fonc & add test filename

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -11,11 +11,45 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int	main (int ac, char *av[])
 {
+	std::string	filename = "";
+	std::string	s1 = "";
+	std::string	s2 = "";
+	
 	if (ac != 4)
+	{
+		std::cerr << "Error: " << av[0] << " <filename> <s1> <s2>" << std::endl;
 		return (EXIT_FAILURE);
-	std::cout << av[0][1] << std::endl;
+	}
+
+	filename = av[1];
+	s1 = av[2];
+	s2 = av[3];
+
+	std::ifstream	inputFile(filename.c_str());
+	if (!inputFile.is_open())
+	{
+		std::cerr << "ERROR: Could not open file " << std::endl;
+		return(EXIT_SUCCESS);
+	}
+
+	std::ofstream	outputFile((filename + ".replace").c_str());
+	if (!outputFile.is_open())
+	{
+		std::cerr << "ERROR : Could not create output file " << std::endl;
+		inputFile.close();
+		return (EXIT_SUCCESS);
+	}
+
+	inputFile.close();
+	outputFile.close();
 	return (EXIT_SUCCESS);
 }
+
+/* ************************************************************************** */
+/*									      */
+/* ************************************************************************** */
