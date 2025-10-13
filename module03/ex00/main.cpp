260bff12edd108cb51aaa1b16b9a7713d5044fbb
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-13 14:57:56 +0200

    write the main

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -10,4 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int main() {
+    ClapTrap claptrap("Robo");
+    claptrap.attack("Enemy");
+    claptrap.takeDamage(5);
+    claptrap.beRepaired(3);
+    return 0;
+}
 
