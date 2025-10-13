ea4de06b5a05eba83378c74aa103d35d52f64d62
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-13 12:19:50 +0200

    write canonique forme

--- module03/ex00/ClapTrap.cpp
+++ module03/ex00/ClapTrap.cpp
@@ -10,4 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+	std::cout << " Default constructor called " std::endl;
+}
+
+ClapTrap::ClapTrap (const std::string& name) : _name(name), _hitPoint(10), _energiePoint(10), _attackDamage(0)
+{
+	std::cout << " ClapTrap robo constructed! " << std::endl;
+}
+
+ClapTrap::ClapTrap (const ClapTrap& other) : _name(other._name), _hitPoint(other._hitPoint), _energiePoint(other._energiePoint), _attackDamage(other._attackPoint)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+ClapTrap	&ClapTrap::operator=(const ClapTrap& other)
+{
+	if (this != &other)
+	{
+		this->_name = &other._name;
+		this->_hitPoint = &other._hitPoint;
+		this->_energiePoint = &other._energiePoint;
+		this->_name = &attackDamage._attackDamage;
+		std::cout << " assignement operator called " << std::endl;
+		return(*this);
+	}
+
+}
 
