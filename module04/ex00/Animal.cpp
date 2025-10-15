b317ae9eefea8708927ac6c5bb6e9c19cb399429
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-15 14:53:14 +0200

    write a fonc of class TODO makesound

--- module04/ex00/Animal.cpp
+++ module04/ex00/Animal.cpp
@@ -10,4 +10,52 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "Animal.hpp"
 
+/* ************************************************************************** */
+
+Animal::Animal( void ) 
+{ 
+	std::cout << "Default constructor called"<< std::endl; 
+}
+
+static Animal::~Animal( void ) 
+{ 
+	std::cout << "Default destructor called"<< std::endl; 
+}
+
+/* ************************************************************************** */
+
+Animal::Animal( const Animal& other ) : _type(other._type) 
+{ 
+	std::cout << "Copy constructor called"<< std::endl; 
+}
+
+Animal&	Animal::operator=(const Animal& other)
+{
+	if (*this != &other)
+	{
+		_type = other._type;
+		std::cout << "assignement operator called " << std::endl;
+	}
+	return (*this);
+}
+
+/* ************************************************************************** */
+
+std::string	getType( void ) const;
+{
+	std::cout << "getType meber fonction called" << std::endl;
+}
+
+void		setType(std::string type) const;
+{
+	return (_type = type);
+}
+
+/* ************************************************************************** */
+
+void	makeSound( void )
+{
+	
+}
