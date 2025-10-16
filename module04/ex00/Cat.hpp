a35a06c6cee176f164d189a4de730acbf8ea9479
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-16 14:02:46 +0200

    write a Cat class

--- module04/ex00/Cat.hpp
+++ module04/ex00/Cat.hpp
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef CAT_HPP
+# define CAT_HPP
 
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+	public :
+		cat( void );
+		static ~cat( void );
+	
+	public :
+		makeSound( void );
+};
+
+
+#endif
