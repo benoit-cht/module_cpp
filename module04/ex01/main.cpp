10a43a6e324abd54df6c803292ca402217dc4075
Benoit Challat <bchallat@f1br7s17.clusters.42paris.fr>
2025-10-19 10:01:16 +0200

    add test of subject in main.cpp

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -13,20 +13,14 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
-int	main()
+int main()
 {
-	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
+	delete j;//should not create a leak
+	delete i;
 
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
 //...
-
 	return (0);
-
 }
