0f5f83df9e30df8930bab43a0eb1c547bb9c7632
Benoit Challat <bchallat@f1br7s17.clusters.42paris.fr>
2025-10-19 12:47:05 +0200

    Update a test in the main.cpp

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -15,13 +15,20 @@
 
 int main()
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	const int arraySize = 4;
+	Animal* animals[arraySize];
 
-	delete j;//should not create a leak
-	delete i;
+	for (int i = 0; i < arraySize; i++)
+	{
+		if (i < arraySize / 2) 
+			animals[i] = new Dog(); 
+		else
+            		animals[i] = new Cat();
+	}
 
-//...
-	return (0);
+	for (int i = 0; i < arraySize; i++)
+        	delete animals[i];
+
+    return EXIT_SUCCESS;
 }
+
