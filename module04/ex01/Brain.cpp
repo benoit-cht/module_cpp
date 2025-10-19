ee9ff89ca19dffa172738ed93e153bdcf2ef171f
Benoit Challat <bchallat@f1br7s17.clusters.42paris.fr>
2025-10-19 10:40:42 +0200

    write all fonction class ideas

--- module04/ex01/Brain.cpp
+++ module04/ex01/Brain.cpp
@@ -10,4 +10,56 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Brain.hpp"
 
+
+/* ************************************************************************** */
+
+Brain::Brain( void )
+{
+	std::cout << "Default Brain constructor called " << std::endl;
+}
+
+virtual	Brain::~Brain( void )
+{
+	std::cout << "Default Brain constructor called" << std::endl;
+}
+
+/* ************************************************************************** */
+
+Brain::Brain(const Brain& other)
+{
+	std::cout << "Brain copy called" << std::endl;
+
+	for (int index, index < 100, index++)
+		this->_ideas[index] = other._ideas[index];
+}
+
+Brain&	Brain::operator=(const Brain& other)
+{
+	std::cout << "Operator assigment called" << std::endl;
+
+	for (int index, index < 100, index++)
+		this->_ideas[index] = other._ideas[index];
+	return (*this);
+}
+
+/* ************************************************************************** */
+
+std::string	getIdeas(unsigned int index) const
+{
+	if ( index >= 100 )
+		std::err << "This ideas is not in the array !" << std::endl;
+	else
+		return (*this->_ideas[index]);
+	return ("\0");
+}		
+
+void		setIdeas(const std::string str, unsigned int index)
+{
+	if (index >= 100 || str == "\0")
+		std::err << "Dont can't change ideas" << std::endl;
+	else
+		*this->_ideas[index] == str;
+}
+/* ************************************************************************** */
