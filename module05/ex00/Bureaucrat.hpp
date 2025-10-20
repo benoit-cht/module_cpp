7b169c754ac8f94fec0db4c923f2bb7addf7c643
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-20 15:03:49 +0200

    write first vertion of class Bureaucrat

--- module05/ex00/Bureaucrat.hpp
+++ module05/ex00/Bureaucrat.hpp
@@ -10,4 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
 
+#include <iostream>
+#include <exeption>
+#include <string>
+
+class Bureaucrat {
+
+	private :
+		std::string	_name;
+		int		_grade;
+
+	public :
+		Bureaucrat( void );
+		virtual ~Bureaucrat ( void );
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrate& operator=(const Bureaucrat &other);
+	
+	public :
+		std::string	getName() const;
+		int		getGrade() const;
+		void		setName();
+		void		setGrade();
+
+	public :
+		class GradeTooHighExeption : public std::expection {
+			const char* what() const throw()
+		};
+
+		class GradeTooLowExeption : public std::exeption {
+			const char* what() const throw()
+		};
+};
+
+# endif 
