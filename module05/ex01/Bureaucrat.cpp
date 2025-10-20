0eb724916b91f4210862bb503fac3c68eae11400
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-20 17:11:55 +0200

    init repository of ex02

---
+++ module05/ex01/Bureaucrat.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchallat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 14:04:20 by bchallat          #+#    #+#             */
+/*   Updated: 2025/10/20 14:04:22 by bchallat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Bureaucrat.hpp"
+
+/* ************************************************************************** */
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name)
+{
+	if (grade < 1)
+		throw	Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw	Bureaucrat::GradeTooLowException();
+	_grade = grade;
+}
+
+Bureaucrat::~Bureaucrat( void ) {}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other): _name(other._name), _grade(other._grade){}
+
+Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& other)
+{
+	if ( this != &other )
+		_grade = other._grade ;
+	return ( *this );
+}
+
+/* ************************************************************************** */
+
+const std::string&	Bureaucrat::getName() const { return (_name); }
+int			Bureaucrat::getGrade() const{ return (_grade); }
+
+/* ************************************************************************** */
+
+void	Bureaucrat::incrementGrade()
+{
+	if (_grade <=1)
+		throw	Bureaucrat::GradeTooHighException();
+	_grade--;
+}
+
+void	Bureaucrat::decrementGrade()
+{
+	if (_grade >= 150)
+		throw	Bureaucrat::GradeTooLowException();
+	_grade++;
+}
+
+/* ************************************************************************** */
+
+const char*	Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ( "Grade is too high !" );
+}
+const char*	Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade is too low !");
+}
+
+/* ************************************************************************** */
+
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat& bureaucrat)
+{
+	os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
+	return ( os );
+}
+
+/* ************************************************************************** */
+/*                                                                            */
+/* ************************************************************************** */
---
+++ module05/ex01/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchallat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 14:04:12 by bchallat          #+#    #+#             */
+/*   Updated: 2025/10/20 14:04:14 by bchallat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include "Bureaucrat.hpp"
+
+int	main ( void )
+{
+	try{ 
+		Bureaucrat alice("Alice", 151); // Trop bas
+	}catch (const std::exception& e){ 
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+
+	try {
+		Bureaucrat bob("Bob", 0); // Trop haut
+	} catch (const std::exception& e) { 
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+	return EXIT_SUCCESS;
+}
