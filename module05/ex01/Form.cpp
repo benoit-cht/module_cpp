6f83e780401c09697081a1b80ec96eff47d671da
Benoit Challat <benoitchallat@macbookpro-1.home>
2025-10-21 17:41:01 +0200

    write fonc of canonique class form

--- module05/ex01/Form.cpp
+++ module05/ex01/Form.cpp
@@ -10,4 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "Form.hpp"
 
+/* ************************************************************************** */
+
+Form::Form(std::string name): _nam(name) , isSigned(false) {}
+
+Form::~Form( void ) {}
+
+
+Form::Form(const Form& other): _name(other._name), isSigned(other.isSigned) {}
+
+Form&	Form::operator=(const Form& other)
+{
+	if ( this != other )
+		_name = other._name, isSigned = other.isSigned ;
+	return ( *this );
+}
+
+/* ************************************************************************** */
+
+std::string	Form::getName() const { return (_name) }
+bool		Form::getIsSigned() const { return (isSigned) }
+
+void		Form::setName(std::string& name) { _name = name; }
+void		Form::setIsSigned(bool value) { isSigned = value; }
+
+/* ************************************************************************** */
+
+std::ostream&	operator<<(std::ostream& os, const Form& form)
+{
+	os << form.getName() << ", form is signed" << form.getIsSigned();
+}
+
+/* ************************************************************************** */
+/*                                                                            */
+/* ************************************************************************** */
