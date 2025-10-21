efbc80621b08873486f898e7b7513c48ede3b0ba
Benoit Challat <benoitchallat@macbookpro-1.home>
2025-10-21 18:19:21 +0200

    implemente exception in class Form

--- module05/ex01/Form.cpp
+++ module05/ex01/Form.cpp
@@ -14,27 +14,53 @@
 
 /* ************************************************************************** */
 
-Form::Form(std::string name): _nam(name) , isSigned(false) {}
+Form::Form(const std::string name, const int gradeTooSign, const int gradeTooExecute)
+	: _nam(name), isSigned(false), _gradeTooSign(gradeTooSign), _gradeTooExecute(gradeTooExecute)
+{
+	if ( gradeTooSign < 1 || gradeTooExecute < 1)
+	{
+		throw GradeTooHighException();
+	}
+	else if (gradeTooSign > 150 || gradeTooExecute > 150)
+	{
+		throw GradeTooLowException();
+	}
+}
 
 Form::~Form( void ) {}
 
-
-Form::Form(const Form& other): _name(other._name), isSigned(other.isSigned) {}
+Form::Form(const Form& other)
+	: _name(other._name), isSigned(other.isSigned), _gradeTooSign(other.gradeTooSign), _gradeTooExecute(other.gradeTooExecute){}
 
 Form&	Form::operator=(const Form& other)
 {
 	if ( this != other )
-		_name = other._name, isSigned = other.isSigned ;
+		isSigned = other.isSigned ;
 	return ( *this );
 }
 
 /* ************************************************************************** */
 
-std::string	Form::getName() const { return (_name) }
-bool		Form::getIsSigned() const { return (isSigned) }
+const std::string&	Form::getName() const { return (_name); }
+bool			Form::getIsSigned() const { return (isSigned); }
 
-void		Form::setName(std::string& name) { _name = name; }
-void		Form::setIsSigned(bool value) { isSigned = value; }
+void			Form::setName(std::string& name) { _name = name; }
+void			Form::setIsSigned(bool value) { isSigned = value; }
+
+const int		Form::getGradeTooSign() const {return (_gradeTooSign); }
+const int		Form::getGradeTooExecute() const { return (_gradeTooExecute); }
+
+/* ************************************************************************** */
+
+const char*	Form::GradeTooHighException::what() const throw()
+{
+	return ("Form grade is too high!");
+}
+
+const char*	Form::GradeTooLowException::what() const throw()
+{
+	return ("Form grade is too low!");
+}
 
 /* ************************************************************************** */
 
--- module05/ex01/Form.hpp
+++ module05/ex01/Form.hpp
@@ -20,23 +20,36 @@
 class Form {
 	
 	public :
-		Form(std::string name);
+		Form(const std::string name, const int gradeToSign, const int gradeToExecute);
 		Form(const Form& other);
 		Form& operator=(const Form& other);
 		virtual ~Form();
 
 	public :
-		std::string	getName() const;
-		void		setName(std::string name);
+		const std::string&	getName() const;
+		void			setName(std::string name);
 
-		bool		getIsSigned() const;
-		bool		setIsSigned(bool value);
+		bool			getIsSigned() const;
+		bool			setIsSigned(bool value);
+		
+		const int		getGradeTooSign() const;
+		const int		getGradeTooExecute() const;
 
 	public :
-		//exception
+		class GradeTooHighExeception : public std::exeception{
+			public :
+				const char* what() const throw();
+		};
+
+		class GradeTooLowExeception : public std::exception{
+			public :
+				const char* what() const throw();
+		};
 
 	private :
-		std::string	_name;
+		const std::string	_name;
+		const int		_gradeTooSign;
+		const int		_gradeTooExecute;
 	
 	protected :
 		bool		isSigned;
