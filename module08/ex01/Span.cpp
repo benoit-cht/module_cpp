2fb17ab670cf7fbb64d2c5fa2e0bdc320ac9d729
Benoit Challat <benoitchallat@macbookpro-1.home>
2025-10-25 18:15:14 +0200

    write all class Span

--- module08/ex01/Span.cpp
+++ module08/ex01/Span.cpp
@@ -10,4 +10,85 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+/* ************************************************************************** */
+
+Span::Span(unsigned int n): _N(n) {}
+
+Span::~Span( void ) {}
+
+/* ************************************************************************** */
+
+Span::Span(const Span& other): _Number(other._Number), _N(other._N) {}
+
+Span&	Span::operator=(const Span& other) 
+{
+	_Number = other._Number;
+	_N = other._N;
+
+	return (*this);
+}
+
+/* ************************************************************************** */
+
+void	Span::addNumber(int newValue)
+{
+	if (_Number.size() >= _N)
+		throw FullSpanException();
+	_Number.push_back(newValue);
+}
+
+unsigned int	Span::shortestSpan() const
+{
+	std::vector<int>	vec = _Number;
+	int			minSpan = 0;
+	int			currSpan = 0;
+
+	if (_N <= 1)
+		throw SmallSpanException();
+	
+	std::sort(vec.begin(), vec.end());
+	minSpan = vec[1] - vec[0];
+
+	for(size_t x = 2; x < _N; x++)
+	{
+		currSpan = vec[x] - vec[x - 1];
+		if (currSpan < minSpan)
+			minSpan = currSpan;
+	}
+	return ( minSpan ) ;
+}
+
+unsigned int	Span::longestSpan() const
+{
+	std::vector<int> vec = _Number;
+
+	if (_Number.size() < 2)
+		throw SmallSpanException();
+	
+	std::sort(vec.begin(), vec.end());
+	return ( vec.back() - vec.front() ) ;
+}
+
+template<typename ITR>
+void	Span::addRange(ITR begin, ITR end)
+{
+	if (_Number.size() + std::distance(begin, end) > _N)
+		throw FullSpanException();
+	_Number.insert(_Number.end(), begin, end);
+}
+
+
+/* ************************************************************************** */
+
+const char*	Span::FullSpanException::what() const throw()
+{
+	return ( "Span is full " ) ;
+}
+
+const char*	Span::SmallSpanException::what() const throw()
+{
+	return ( "Span is small ") ;
+}
 
--- module08/ex01/Span.hpp
+++ module08/ex01/Span.hpp
@@ -10,4 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef SPAN_HPP
+# define SPAN_HPP
 
+# include <exception>
+# include <algorithm>
+# include <vector>
+
+class Span {
+	
+	public :
+		Span(unsigned int n);
+		Span(const Span& other);
+		Span& operator=(const Span& other);
+		virtual ~Span( void );
+
+	public :
+		void		addNumber(int newValue);
+		unsigned int	shortestSpan() const;
+		unsigned int	longestSpan() const;
+		template<typename ITR>
+		void		addRange(ITR begin, ITR end);
+
+	public :
+		class FullSpanException: public std::exception 
+		{
+			const char* what() const throw();
+		};
+
+		class SmallSpanException: public std::exception
+		{
+			const char* what() const throw();
+		};
+
+	private :
+		unsigned int		_N;
+		std::vector<int>	_Number;
+};
+
+# endif
