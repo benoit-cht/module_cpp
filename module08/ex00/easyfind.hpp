08b384990308ff218f3564148f238e47aab58d5e
Benoit Challat <benoitchallat@macbookpro-1.home>
2025-10-25 16:19:39 +0200

    write ex00

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -10,4 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+# include <stdexcept>
+
+template<typename T>
+typename	T::iterator easyfind(T& container, int n)
+{
+	typename T::iterator	iterator = std::find(container.begin(), container.end(), n) ;
+
+	if (iterator == container.end())
+		throw std::runtime_error("Value not fond in container") ;
+
+	return ( iterator ) ;
+}
+
+# endif
+
+/* ************************************************************************** */
+/*                                                                            */
+/* ************************************************************************** */
 
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -10,4 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "easyfind.hpp"
+# include <vector>
+# include <list>
+# include <iostream>
 
+int	main ()
+{
+	std::vector<int>	vec = {1, 2, 3, 4, 5};
+	std::list<int>		lis = {5, 10, 15, 20};
+
+	try
+	{
+		std::vector<int>::iterator	itv = easyfind(vec, 5);
+		std::list<int>::iterator	itl = easyfind(lis, 5);
+		std::cout << "Found<vector>: " << *itv << std::endl;
+		std::cout << "Found<list>  : " << *itl << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::vector<int>::iterator itv = easyfind(vec, 0);
+		std::list<int>::iterator itl = easyfind(lis, 0);
+		std::cout << "Found<vector>: " << *itv << std::endl;
+		std::cout << "Found<list>  : " << *itl << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+
+	return EXIT_SUCCESS ;
+}
