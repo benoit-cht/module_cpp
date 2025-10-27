61a4f54e3e44874a3550751c7766972b1343112a
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-27 16:45:35 +0100

    update class intern

--- module05/ex03/Intern.hpp
+++ module05/ex03/Intern.hpp
@@ -27,7 +27,7 @@ class Intern {
 		virtual ~Intern( void );
 	
 	public :
-		makeForm(std::string name, std::string target) const ; 
+		AForm*	makeForm(std::string name, std::string target) const ; 
 };
 
 # endif
