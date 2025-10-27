ad7eba0e3393d2eda5e7586fdb72794b93503e07
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-27 09:25:40 +0100

    implement Wrong class

--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -13,19 +13,42 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
+# include "WrongCat.hpp"
+# include "WrongDog.hpp"
+
 int	main()
 {
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 
+	const WrongAnimal* Wmeta = new WrongAnimal();
+	const WrongAnimal* Wj = new WrongDog();
+	const WrongAnimal* Wi = new WrongCat();
+
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
+	
+	std::cout << Wj->getType() << " " << std::endl;
+	std::cout << Wi->getType() << " " << std::endl;
+
 
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
+
+	Wi->makeSound(); //will output the cat sound!
+	Wj->makeSound();
+	Wmeta->makeSound();
+
 //...
+	delete meta;
+	delete i;
+	delete j;
+
+	delete Wmeta;
+	delete Wi;
+	delete Wj;
 
 	return (0);
 
