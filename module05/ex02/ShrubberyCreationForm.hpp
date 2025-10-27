5142fc1a2874d7414d86aa4d5d9f3383e73679a5
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-27 13:31:43 +0100

    write a class ShrubberyCreationForm

--- module05/ex02/ShrubberyCreationForm.hpp
+++ module05/ex02/ShrubberyCreationForm.hpp
@@ -10,4 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
 
+# include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm {
+	
+	public :
+		ShrubberyCreationForm( void );
+		ShrubberyCreationForm(const  ShrubberyCreationForm& other);
+		ShrubberyCreationForm& operator=(const  ShrubberyCreationForm& other);
+		virtual ~ShrubberyCreationForm( void );
+
+	public :
+		virtual void execute(const Buraucrat& executor) const ;
+
+	private :
+		std::string	_Target;
+
+};
+
+#endif
