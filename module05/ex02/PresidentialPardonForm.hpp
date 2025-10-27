4e7c6e1a948ba81bb2a9b559de0469813e2a0bec
Benoit Challat <bchallat@f6r3s4.clusters.42paris.fr>
2025-10-27 13:54:45 +0100

    write class PresidentialPardonForm

--- module05/ex02/PresidentialPardonForm.hpp
+++ module05/ex02/PresidentialPardonForm.hpp
@@ -10,4 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
 
+# include "AForm.hpp"
+
+class PresidentialPardonForm: public AForm {
+	
+	public :
+		PresidentialPardonForm( void );
+		PresidentialPardonForm(const PresidentialPardonForm& other);
+		PresidentialPardonForm& operator=(const PresidentialPardonForm& other);
+		virtual ~PresidentialPardonForm( void );
+
+	public :
+		virtual void	execute(const Buraucrat& executor) const ;
+	
+	private :
+		std::string	_Target;
+};
+
+# endif
