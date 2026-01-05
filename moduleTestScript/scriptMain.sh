e2ac9131bb0fd98822e47a228b1c431f38be188e
Benoit Challat <bchallat@f4r3s12.clusters.42paris.fr>
2026-01-05 14:26:09 +0100

    update sript test

--- moduleTestScript/scriptMain.sh
+++ moduleTestScript/scriptMain.sh
@@ -18,13 +18,13 @@ for i in {0..8}; do
   echo -e "${BLUE}\n[ Module0$i ]_____[compilation]_____[Valgrind]_____[clean]${NC}"
 
   if [[ "$i" -eq 0 ]]; then
-    (cd "./module0$i" && ../testScript/unitScript/unitModule00.sh) || {
+    (cd "./module0$i" && ../moduleTestScript/unitScript/unitModule00.sh) || {
       echo -e "\033[0;31mErreur : Impossible d'exécuter le test dans module0$i\033[0m"
     }
   # Vérifie que le répertoire existe avant de s'y déplacer
   else
     if [ -d "./module0$i" ]; then
-      (cd "./module0$i" && ../testScript/sriptTest.sh) || {
+      (cd "./module0$i" && ../moduleTestScript/sriptTest.sh) || {
         echo -e "\033[0;31mErreur : Impossible d'exécuter le test dans module0$i\033[0m"
       }
     else
