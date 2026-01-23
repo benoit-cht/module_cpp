c7dda72170df979532e89995d13d5209d42e06eb
Benoit Challat <bchallat@f6r4s1.clusters.42paris.fr>
2026-01-23 11:02:22 +0100

    norm

--- module07/ex00/watever.hpp
+++ module07/ex00/watever.hpp
@@ -13,22 +13,19 @@
 # ifndef WATEVER_HPP
 # define WATEVER_HPP
 
-template<typename T>
-void	swap(T &a, T&b)
+template<typename T> void	swap(T &a, T&b)
 {
 	T	swap = a;
 	a = b;
 	b = swap;
 }
 
-template<typename T>
-T const &min(T const &a, T const &b)
+template<typename T> T const &min(T const &a, T const &b)
 {
 	return ((a < b) ? a : b);
 }
 
-template<typename T>
-T const &max(T const &a, T const &b)
+template<typename T> T const &max(T const &a, T const &b)
 {
 	return ((a > b) ? a : b);
 }
