5efbbed37352df7acaa7eb0d14594d60f73782fd
benoit-cht <benoit.challat@gmail.com>
2026-05-07 21:07:17 +0200

    finsh ex 02

--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -6,7 +6,120 @@
 /*   By: ton_utilisateur_42 <ton_email@student.42.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 09:31:42 by ton_utilisate     #+#    #+#             */
-/*   Updated: 2025/11/21 09:31:43 by ton_utilisate    ###   ########.fr       */
+/*   Updated: 2026/05/07 21:03:12 by bchallat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP_INCLUDED
+#define PMERGEME_HPP_INCLUDED
+
+#include <iostream>
+
+template<typename Container>
+class PmergeMe {
+
+  public:
+    PmergeMe( void );
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    virtual   ~PmergeMe( void );
+
+  public:
+    Container merge(const Container& raw_cont);
+    Container fusion(const Container& cont1, const Container& cont2);
+  
+  private:
+    Container _container;
+
+};
+
+/* ============================================================================= */
+/*    DÉFINITIONS DES MÉTHODES (doivent être dans le .hpp en C++98)              */
+/* ============================================================================= */
+
+template<typename Container>
+PmergeMe<Container>::PmergeMe(void) {}
+
+template<typename Container>
+PmergeMe<Container>::~PmergeMe(void) {}
+
+/* ************************************************************************** */
+
+template<typename Container>
+PmergeMe<Container>::PmergeMe(const PmergeMe<Container>& other) { *this = other; }
+
+template<typename Container>
+PmergeMe<Container>& PmergeMe<Container>::operator=(const PmergeMe<Container>& other) 
+{
+    if (this != &other) 
+    {
+        this->_container = other._container;
+    }
+    return ( *this );
+}
+
+/* ************************************************************************** */
+
+template<typename Container>
+Container PmergeMe<Container>::fusion(const Container& cont1, const Container& cont2)
+{
+  Container fusion;
+  typename  Container::const_iterator itr1 = cont1.begin();
+  typename  Container::const_iterator itr2 = cont2.begin();
+
+  while (itr1 != cont1.end() && itr2 != cont2.end())
+  {
+    if ( *itr1 < *itr2 )
+    {
+      fusion.push_back(*itr1);
+      itr1++;
+    }
+    else
+    {
+      fusion.push_back(*itr2);
+      itr2++;
+    }
+  }
+  
+  while (itr1 != cont1.end())
+  {
+    fusion.push_back(*itr1);
+    itr1++;
+  }
+
+  while (itr2 != cont2.end())
+  {
+    fusion.push_back(*itr2);
+    itr2++;
+  }
+
+  return ( fusion );
+}
+
+template<typename Container>
+Container PmergeMe<Container>::merge(const Container& cont)
+{
+  if (cont.size() <= 1)
+  {
+    return cont;
+  }
+
+  typename Container::const_iterator it = cont.begin();
+  typename Container::const_iterator mid = cont.begin();
+  std::advance(mid, cont.size() / 2);
+
+  Container cont1(it, mid);
+  Container cont2(mid, cont.end());
+
+  cont1 = this->merge(cont1);
+  cont2 = this->merge(cont2);
+
+  return ( this->fusion(cont1, cont2) );
+}
+
+#endif // !PMERGEME_HPP_INCLUDED
+
+/* ************************************************************************** */
 /*                                                                            */
 /* ************************************************************************** */
 
--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -6,7 +6,129 @@
 /*   By: ton_utilisateur_42 <ton_email@student.42.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 09:31:36 by ton_utilisate     #+#    #+#             */
-/*   Updated: 2025/11/21 09:31:38 by ton_utilisate    ###   ########.fr       */
+/*   Updated: 2026/05/07 21:05:40 by bchallat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <vector>
+#include <list>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+
+#include "PmergeMe.hpp"
+
+template<typename T>
+static void print_cont(T& cont, std::string msg);
+static int string_to_int(const std::string& s);
+static bool is_positive_integer(const std::string& s);
+static bool parsing(int ac, char *av[], std::vector<int>& vec);
+
+/* ============================================================================= */
+/*                                                                               */
+/* ============================================================================= */
+
+int main(int ac, char *av[])
+{
+  std::vector<int>            vec;
+  PmergeMe<std::vector<int>>  pmerge_vec;
+  PmergeMe<std::list<int>>    pmerge_lst; 
+
+  if( ac < 2 || !parsing(ac, av, vec))
+  {
+    std::cerr << "Error : bad argument." << std::endl;
+    return ( EXIT_FAILURE );
+  }
+  std::list<int> lst(vec.begin(), vec.end());
+  
+  std::clock_t start_vec = std::clock();
+  std::vector<int> sorted_vec = pmerge_vec.merge(vec);
+  std::clock_t end_vec = std::clock();
+  double time_vec = (double)(end_vec - start_vec) * 1000000.0 / CLOCKS_PER_SEC;
+
+  std::clock_t start_lst = std::clock();
+  std::list<int> sorted_lst = pmerge_lst.merge(lst);
+  std::clock_t end_lst = std::clock();
+  double time_lst = (double)(end_lst - start_lst) * 1000000.0 / CLOCKS_PER_SEC;
+
+ 
+  print_cont(vec, "Before: ");
+  print_cont(sorted_vec, "After: ");
+
+  std::cout << "Time to process a range of " << vec.size() 
+            << " elements with std::vector: " << time_vec << " us" << std::endl;
+  
+  std::cout << "Time to process a range of " << lst.size() 
+            << " elements with std::list: " << time_lst << " us" << std::endl;
+
+  return ( EXIT_SUCCESS );
+
+}
+
+/* ============================================================================= */
+/*                       DÉFINITIONS DES FONCTION STATIC                         */
+/* ============================================================================= */
+
+
+static bool is_positive_integer(const std::string& s)
+{
+    size_t i = 0;
+
+    if ( s.empty() ) 
+      return false;
+
+    if (s[0] == '+') 
+      i = 1;
+    
+    if (i == s.size()) 
+      return (false);    
+
+    while (i < s.size()) 
+    {
+        if (!isdigit(s[i])) 
+          return (false);
+        i++;
+    }
+    
+    return (true);
+}
+
+static int string_to_int(const std::string& s)
+{ 
+  return (static_cast<int>(strtol(s.c_str(), NULL, 10)));
+}
+
+static bool parsing(int ac, char *av[], std::vector<int>& vec)
+{
+  for (int i = 1; i < ac; i++) 
+  {
+    std::string arg = av[i];
+    if (!is_positive_integer(arg)) 
+    {
+      std::cerr << "Error " << std::endl;
+      return (false);
+    }
+    
+    int num = string_to_int(arg);
+    vec.push_back(num);
+  }
+  
+  return (true);
+}
+
+template<typename T>
+static void print_cont(T& cont, std::string msg)
+{
+  std::cout << msg;
+  for (size_t i = 0; i < cont.size(); i++) 
+  {
+    std::cout << cont[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+/* ************************************************************************** */
+/*                                                                            */
+/* ************************************************************************** */
