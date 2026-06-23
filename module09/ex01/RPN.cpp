a5adb37e62f91ef0844d402766ff709ba7b61e46
bchallat <bchallat@f6r5s1.paris.42.school>
2026-06-23 16:15:59 +0200

    implemente a metode of class

--- module09/ex01/RPN.cpp
+++ module09/ex01/RPN.cpp
@@ -10,3 +10,82 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ========================================================================== */
+/*                                                                            */
+/* ========================================================================== */
+
+RPN::RPN( void ){ std::cout << "RPN: Call constructor" << std::endl}
+
+RPN::~RPN( void ){ std::cout << "RPN:" Call destructor << std::endl}
+
+RPN::RPN(const RPN& other): _stack(other._stack), _result(other._result) {}
+
+RPN::RPN(const RPN& other)  {
+
+  _stack = other._stack;
+  _result = other.result;
+  return( *this );
+}
+
+/* ========================================================================== */
+/*                                                                            */
+/* ========================================================================== */
+
+int             RPN::getResult( void ) const { return(_result); }
+std::stack<int> RPN::getStack( void ) const { return(_stack); }
+
+void            RPN::setResult(int number) { _result = number; }
+
+/* ========================================================================== */
+
+static bool   isAvalidInput(std::string str) {
+
+  if (str != '+' || str != '-' || str != '*' || str != '/' || !isdigit( atoi(line.c_str()) )) {
+
+    return ( false );
+  }
+  return ( true );
+}
+
+void            RPN::setStack(std::string& input) {
+
+  for (std::string::iterator itr = input.begin(); itr < input.end(); itr++) {
+
+    if (!isAvalidInput(itr))  {
+
+      trow badCharacterException();
+    } else {
+
+      _stack.push_back(itr);
+    }
+  }
+}
+
+/* ========================================================================== */
+
+
+//int           RPN::evaluate( void );
+
+
+/* ========================================================================== */
+/*                                                                            */
+/* ========================================================================== */
+
+const char* RPN::divisionByZeroException::what() const trow() {
+
+  return ("Division by zero is Impossible !");
+}
+
+const char* RPN::operatorToNeedException::what() const trow {
+
+  return ("Need a operator or Number to valid notation !");
+}
+
+const char* RPN::badCharacterException::what() const trow() {
+
+  return (" a character input is incorect !");
+}
+
+/* ========================================================================== */
+/*                                                                            */
+/* ========================================================================== */
